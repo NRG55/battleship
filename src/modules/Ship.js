221585e5175725f6a1f53c2a2ee2b3cfcb35baa7
NRG55 <maloj_1980@yahoo.fr>
2025-02-27 09:31:28 +0000

    Add(Ship.js): hit(), isSunk() methods. Add(Ship.test.js): tests for hit() and isSunk() methods.

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,5 +1,24 @@
 export default class Ship {
+    #hits = 0;
+    #sunk = false;
+
     constructor(length) {
-        this.length = length;
+        this.length = length;        
+    };   
+
+    isSunk() {
+        if (this.#hits === this.length) {
+            this.#sunk = true;
+        };
+
+        return this.#sunk;
+    };
+
+    hit() {
+        if (this.#hits < this.length) {
+            this.#hits++;           
+        };
+
+        return this.#hits;
     };
 };
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,9 +1,19 @@
 import Ship from "../modules/Ship.js";
 
 describe('Ship', () => {
-    let ship = new Ship(1);
+    let ship = new Ship(1);     
+    
+    ship.hit();
 
-    test("ship length", () => {
+    test('length', () => {
         expect(ship.length).toBe(1);
+    });     
+
+    test('add hit', () => {
+        expect(ship.hit()).toBe(1);
+    });
+
+    test('is sunk', () => {
+        expect(ship.isSunk()).toBe(true);
     });
 });

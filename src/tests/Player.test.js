ae30eca3f8ce23b983d5fd755d5cc51a1c120d28
NRG55 <maloj_1980@yahoo.fr>
2025-03-04 02:20:30 +0000

    Create: Player.js class and Player.test.js file. Add(Player.js): Player class. Add(Player.test.js): players properties tests.

---
+++ src/tests/Player.test.js
@@ -0,0 +1,37 @@
+import Player from "../modules/Player.js";
+import Gameboard from "../modules/Gameboard.js";
+
+describe('Player', () => {
+    describe('human', () => {
+        let playerHuman; 
+
+        beforeEach(() => {
+            // by default player type property set to 'human' 
+            playerHuman = new Player();
+        });
+
+        test('property set to - human', () => {
+            expect(playerHuman.type).toBe('human');
+        });
+
+        test('property gameboard', () => {
+            expect(playerHuman.gameboard).toBeInstanceOf(Gameboard);
+        });
+    });
+
+    describe('computer', () => {
+        let playerComputer; 
+
+        beforeEach(() => {
+            playerComputer = new Player('computer');
+        });
+
+        test('property set to - human', () => {
+            expect(playerComputer.type).toBe('computer');
+        });
+
+        test('property gameboard', () => {
+            expect(playerComputer.gameboard).toBeInstanceOf(Gameboard);
+        });
+    });    
+});
